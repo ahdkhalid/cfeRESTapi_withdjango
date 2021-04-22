7cb75fff71b8437d8237f63836f5729c3dcee1c8
ahdkhalid <ahdkhalid@yahoo.com>
2021-04-22 18:38:04 +0430

    Testing other serializer fields

--- src/status/api/serializers.py
+++ src/status/api/serializers.py
@@ -10,11 +10,22 @@ Serializers -> Validate data
 
 class StatusSerializer(serializers.ModelSerializer):
     uri               = serializers.SerializerMethodField(read_only=True)
-    user              = UserPublicSerializer(read_only=True)
+    #user            = serializers.SerializerMethodField(read_only=True)
+    user            = UserPublicSerializer(read_only=True)
+    # user_id         = serializers.PrimaryKeyRelatedField(source='user', read_only=True)
+    # user_id         = serializers.HyperlinkedRelatedField(
+    #                         source='user',  # user foreign key
+    #                         lookup_field='username',
+    #                         view_name='api-user:detail',
+    #                         read_only=True
+
+    #                         )
+    # user            = serializers.SlugRelatedField(read_only=True, slug_field='username')
     class Meta:
         model = Status
         fields =[
             'uri',
+            # 'user_id',
             'id', # ?? tmp for now,
             'user',
             'content',
